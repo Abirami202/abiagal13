ae268765dec7f49ac7c409f454997536313f24cd
Abirami <131868796+Abirami202@users.noreply.github.com>
2023-09-14 13:54:38 +0530

    Add files via upload

---
+++ pgm3/largestnumber.py
@@ -0,0 +1,14 @@
+# Python program to find largest of two numbers
+ 
+# Input two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+ 
+# Check whether num1 is greater than num2 or not
+if num1 > num2:
+    max_num = num1
+else:
+    max_num = num2
+ 
+# Print the largest number
+print("The largest number is:", max_num)
